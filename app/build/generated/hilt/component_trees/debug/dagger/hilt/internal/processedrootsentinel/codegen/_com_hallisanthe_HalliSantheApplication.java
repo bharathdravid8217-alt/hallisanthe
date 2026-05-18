76c937401372ad79175c2ae8fa89c14a4cda79f7
Halli Santhe <halli@example.com>
2026-05-18 14:55:42 +0530

    Clean up: Remove generated build files to fix language stats

--- app/build/generated/hilt/component_trees/debug/dagger/hilt/internal/processedrootsentinel/codegen/_com_hallisanthe_HalliSantheApplication.java
+++
@@ -1,9 +0,0 @@
-package dagger.hilt.internal.processedrootsentinel.codegen;
-
-import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
-
-@ProcessedRootSentinel(
-    roots = "com.hallisanthe.HalliSantheApplication"
-)
-public final class _com_hallisanthe_HalliSantheApplication {
-}
